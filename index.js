925dc7be8bb007af819a8419264bf450b13494f6
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2023-12-15 14:26:29 -0300

    codigo funcionando
    1. Sanitização e validação dos dados de entrada;
    
    2. Correção da lógica de votação para que verifique se o registro do animal existe antes de adicionar um voto.
    
    3 Implementação e tratamento de erros de maneira adequada, sem vazar detalhes de implementação.
    
    4. Implementação todos os métodos que possuem assinatura no código. (POST de cats and dogs, POST (update) de votos e GET de cats and dogs)

--- index.js
+++ index.js
@@ -5,41 +5,113 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const { body, param, validationResult } = require('express-validator');
+
+
 app.use(bodyParser.json());
 
 
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
-  db.run("CREATE TABLE cats (id INT, name TEXT, votes INT)");
-  db.run("CREATE TABLE dogs (id INT, name TEXT, votes INT)");
+  db.run("CREATE TABLE cats (id INTEGER PRIMARY KEY, name TEXT, votes INTEGER)");
+  db.run("CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, votes INTEGER)");
 });
 
-app.post('/cats', (req, res) => {
+// healthy check
+app.get('/', (req, res) => {
+  try {
+    res.status(200).send("Healthy!")
+  } catch(err) {
+    res.status(400).send("Erro da Clarinha")
+  }
+})
+
+const createValidation = [
+  body('name').notEmpty().trim(),
+];
+
+
+app.post('/cats', createValidation, (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send("Nome inválido!");
+  }
+
   const name = req.body.name;
   db.run(`INSERT INTO cats (name, votes) VALUES ('${name}', 0)`, function(err) {
     if (err) {
-      res.status(500).send("Erro ao inserir no banco de dados");
+      res.status(500).send("Erro ao criar o gato");
     } else {
       res.status(201).json({ id: this.lastID, name, votes: 0 });
     }
   });
 });
 
-app.post('/dogs', (req, res) => {
-  
+app.post('/dogs', createValidation, (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send("Nome inválido!");
+  }
+
+  const name = req.body.name;
+  db.run(`INSERT INTO dogs (name, votes) VALUES ('${name}', 0)`, function(err) {
+    if (err) {
+      res.status(500).send("Erro ao criar o cachorro");
+    } else {
+      res.status(201).json({ id: this.lastID, name, votes: 0 });
+    }
+  });
 });
 
-app.post('/vote/:animalType/:id', (req, res) => {
- 
-  db.run(`UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`);
-  res.status(200).send("Voto computado");
+
+const voteValidation = [
+  param('animalType').isIn(['cats', 'dogs']),
+  param('id').isInt({ min: 1 }),
+];
+
+app.post('/vote/:animalType/:id', voteValidation, (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send("Erro ao computador o voto, verifique os parametros utilizados");
+  }
+
+  const animalType = req.params.animalType;
+  const id = req.params.id;
+  console.log(animalType);
+  console.log(id);
+
+// Verifica se o id existe no banco de dados, antes de add o voto
+    const verificacao = `SELECT id FROM ${animalType} WHERE id = ${id}`;
+
+    db.get(verificacao, (err, row) => {
+    if (err) {
+      res.status(500).send("Esse animal não existe");
+    } else if (!row) {
+      res.status(404).send("Esse animal não existe");
+    } else {
+
+      // Se o id for encontrado no banco:
+      const updateQuery = `UPDATE ${animalType} SET votes = votes + 1 WHERE id = ${id}`;
+
+      db.run(updateQuery, function(updateErr) {
+        if (updateErr) {
+          res.status(500).send("Erro ao realizar o voto");
+        } else {
+          res.status(200).send("Voto computado");
+        }
+      });
+    }
+  });
 });
 
 app.get('/cats', (req, res) => {
   db.all("SELECT * FROM cats", [], (err, rows) => {
     if (err) {
-      res.status(500).send("Erro ao consultar o banco de dados");
+      res.status(500).send("Erro ao buscar pela lista de gatos");
     } else {
       res.json(rows);
     }
@@ -47,7 +119,13 @@ app.get('/cats', (req, res) => {
 });
 
 app.get('/dogs', (req, res) => {
-  
+  db.all("SELECT * FROM dogs", [], (err, rows) => {
+    if (err) {
+      res.status(500).send("Erro ao buscar pela lista de cachorros");
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.use((err, req, res, next) => {
